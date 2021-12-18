77c352da0652d7893807197524e7101c4e5ad7e8
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2021-12-17 18:26:42 -0800

    Create two_sum_version2.js
    //We are creating a function(not the entire program) for the problem below
    //Input: nums = [2,7,11,15] with target=9
    //Output: [0,1] because nums[0]+nums[1]==target==9
    //the solution below has time comlexity of O(n)

---
+++ two_sum_version2.js
@@ -0,0 +1,22 @@
+//We are creating a function(not the entire program) for the problem below
+//Input: nums = [2,7,11,15] with target=9
+//Output: [0,1] because nums[0]+nums[1]==target==9
+//the solution below has time comlexity of O(n)
+
+var twoSum = function(nums, target)
+{
+  const numsMap={};                                       //creating a constant variable for mapping out the numbers
+  for(let position=0; position<nums.lenght; position++)
+  {
+    const numsMapValue=numsMap[nums[position]];
+        if(numsMapValue>=0)
+        {
+          returns [numsMapValue, position];
+        }
+        else
+        {
+         const findNum = target - nums[position];
+         numsMap[findNum] = position;
+        }
+  }
+};
