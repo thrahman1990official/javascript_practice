5333cc7ece80178da66ddb2382aa958f70f9ed68
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2022-07-30 01:39:33 -0700

    Update javascriptTutorial4.js

--- javascriptTutorial4.js
+++ javascriptTutorial4.js
@@ -1,15 +1,24 @@
 //Normal Function in JavaScript vs ES6 features Arrow function
-function sum(a,b)
+function sum(a, b) //has two parameters
 {
  return a + b; 
 } //Function
+//above is example of the older ES6
 
-function isGood(number)
+LET sum2 = (a, b) => a + b;
+//above is example of the latest ES6
+
+function isGood(number) //has one parameter
 {
  return number >= 0; 
 } //Function
+//above is example of the older ES6
 
-function randomFigure()
+LET isGood2 = (number) => number >= 0;
+//es6
+//above is example of the latest ES6
+
+function randomFigure() //has no parameter
 {
  return Math.random; 
 } //Function
