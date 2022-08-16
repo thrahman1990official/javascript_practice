89b978ec063110b4f5f13cf519ff9b048af737de
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2022-08-16 02:06:37 -0700

    Update JavascriptSession7Part2.js

--- JavascriptSession7Part2.js
+++ JavascriptSession7Part2.js
@@ -1,1 +1,17 @@
+//Challenge 1: Algorithm Sort
+//Implement Bubble Sort
+//This is among several challenges on sorting algorithms
+//Give array of unsorted items, we want to be able to return sorted array, we will see several different methods to do this & learn some trade-offs between
+//between these different approaches. While most modern languages have languages have built-in-sorting methods for applications like this, it is still
+//important to understand some of common basic approaches & learn how they can be implemented. 
+
+//Here we shall bubble-sort. The bubble sort method starts at beginning of an unsorted array & bubbles up unsorted value towards the end, iterating through
+//the array until it is completely sorted it does this by comparing adjacent items & swapping them if they are out of order. The method continues looping
+//through array until no swaps occur at which point array is sorted.
+
+//This method requires multiple iterations through array & for average & worst cases has quadratic time complexity. While simple it is usually impractical
+//in most situations
+
+//Three successful tips of passing Algorithms: (1) Understand sequence (2) Understand flow of algo (3) Understand the statement
+
 
