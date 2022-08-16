a2e406e81182cca720a89edd424814a3ce0af6d8
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2022-08-15 23:55:31 -0700

    Update JavascriptSession7Part1.js

--- JavascriptSession7Part1.js
+++ JavascriptSession7Part1.js
@@ -2,9 +2,14 @@ const strings = ['a', 'b', 'c', 'd'];
 //PUSH
 strings.push('e');
 console.log(strings);
+//Initial array: ['a', 'b', 'c', 'd'];
+//Output array: ['a', 'b', 'c', 'd', 'e'];
+
 //POP
 strings.pop();
 console.log(strings);
+
+
 //UNSHIFT
 strings.unshift('ralph');
 console.log(strings);
