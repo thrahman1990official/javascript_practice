44ba0d26748264efece6f53ecbe0c9b3e38c1186
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2022-08-16 21:09:21 -0700

    Update JavascriptSession7Part2.js

--- JavascriptSession7Part2.js
+++ JavascriptSession7Part2.js
@@ -19,7 +19,7 @@ function bubbleSort(array)
  //change code below
  for(let i = 0; i < array.length -1; i++)
   {
-  for(let j = 0; j < array.length -1; j++)
+  for(let j = 0; j < array.length -1 -i; j++)
    {
     if(array[j] > array[j+1])
      {
