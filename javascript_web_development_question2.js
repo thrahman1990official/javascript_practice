21cbd15cddc0ff600c33a08435f68a431f454f9c
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2022-09-28 19:17:52 -0700

    Update javascript_web_development_question2.js

--- javascript_web_development_question2.js
+++ javascript_web_development_question2.js
@@ -2,9 +2,16 @@
 /* let arr = [1, 2, 3]; */
 /* complete missing JavaScript code on this line */
 /* console.log(arr); */
-/* outputs [1, 2, 3, 4] */
+/* output to the above console: [1, 2, 3, 4] */
 /* Options: */
 /* (1) arr[arr.length + 1] = value */
 /* (2) arr[arr.length] = value */
 /* (3) arr[arr.length - 1] = value */
 /* (4) arr = arr + value */
+
+let arr = [1, 2, 3];
+
+console.log(arr)
+
+/*  Correct answer is number 2 */
+arr[arr.length] = 4
