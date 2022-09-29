e57431f2b2e61b81895faf7d2b9f0137e92a2f04
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2022-09-28 18:36:59 -0700

    Create javascript_web_development_question.js

---
+++ javascript_web_development_question.js
@@ -0,0 +1,16 @@
+/* Given two functions below, what will be there output? Select most accurate response */
+/* Options: (1) Yes (2) No (3) foo1 will give run time error (4) foo2 will give run time error */
+
+const foo1 = () =>
+{
+  {
+    bar: "foo1"
+  };
+}
+
+const foo2 = () =>
+{
+  {
+    bar: "foo2"
+  };
+}
