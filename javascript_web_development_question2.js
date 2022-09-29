5cdffc58887065133287dbe453c001cee77db593
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2022-09-28 19:06:00 -0700

    Update javascript_web_development_question2.js

--- javascript_web_development_question2.js
+++ javascript_web_development_question2.js
@@ -1,4 +1,10 @@
+/* The question is: How to append an element to an array in JavaScript? */
 /* let arr = [1, 2, 3]; */
 /* complete missing JavaScript code on this line */
 /* console.log(arr); */
 /* outputs [1, 2, 3, 4] */
+/* Options: */
+/* (1) arr[arr.length + 1] = value */
+/* (2) arr[arr.length] = value */
+/* (3) arr[arr.length - 1] = value */
+/* (4) arr = arr + value */
