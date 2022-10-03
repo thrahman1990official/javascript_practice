bc40be5026c5b9156531476f838618b18199aefd
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2022-10-03 02:03:11 -0700

    Create javascript_web_development_question3.js

---
+++ javascript_web_development_question3.js
@@ -0,0 +1,7 @@
+// How to initialize new array in JavaScript?
+
+// Options:
+// let txt = new Array("tom","jerry","jim");
+// let txt = new Array"tom","jerry","jim"
+// let txt = new Array: 1=("tom")2=("jerry")3=("jim")
+// let txt = new Array(1:"tom",2:"jerry",3:"jim")
