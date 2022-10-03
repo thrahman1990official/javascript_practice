23fc4c4aa459b2af5eab3c37c3fe10c1586aac6e
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2022-10-03 02:08:07 -0700

    Update javascript_web_development_question3.js

--- javascript_web_development_question3.js
+++ javascript_web_development_question3.js
@@ -5,3 +5,11 @@
 // let txt = new Array"tom","jerry","jim"
 // let txt = new Array: 1=("tom")2=("jerry")3=("jim")
 // let txt = new Array(1:"tom",2:"jerry",3:"jim")
+
+//the first option is a valid option
+
+let txt = new Array("tom","jerry","jim")
+console.log(txt)
+
+// it will produce valid output
+// ['tom','jerry','jim']
